0f20cc4b37972c98b13fe11d9e6e9eca3d6b5dde
Rakin <salmankaderrakin@gmail.com>
2017-04-30 10:28:32 +0600

    updating output context more

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def makeWebhookResult(data):
     # print(json.dumps(item, indent=4))
 
     speech = " Hello!! Today the weather in " + location.get('city') + " is : " + condition.get('text') + \
-             ", and the temperature is " + condition.get('temp') + " " + units.get('temperature') + "Thanks!!"
+             ", and the temperature is " + condition.get('temp') + " " + units.get('temperature') + ".  Thanks!!"
 
     print("Response:")
     print(speech)
