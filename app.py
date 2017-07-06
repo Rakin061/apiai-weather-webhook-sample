9f42dd117d45f76d213cf20754099c6977fb9883
Rakin <salmankaderrakin@gmail.com>
2017-07-06 18:16:28 +0600

    my commit w6

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def processRequest(req):
         parameters = result.get("parameters")
         id = str(parameters.get("Numbers"))
 
-        id = id.strip()
+        
 
         baseurl = "http://10.11.201.93:8086/BotAPI/ApplicationStatus?"
         yql_query="SELECT APPL_STATUS_DESC FROM ocasmn.vw_appl_sts_info WHERE APPLICATION_ID='"+id+"'"
