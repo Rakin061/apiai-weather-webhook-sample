8d0747a959c7f91e151c573f29f81f72af6ea807
Rakin <salmankaderrakin@gmail.com>
2017-08-07 17:40:39 +0600

    Individual perf 18

--- app.py
+++ app.py
@@ -406,14 +406,15 @@ def processRequest(req):
 
         no_of_rows = data["Number of Rows"]
 
-        final_speech = ""
+        speech_counter = ""
 
         for i in range(1, no_of_rows + 1):
-            final_speech = final_speech + " User ID: " + data['Query'][0][0]['Row' + str(no_of_rows)]['USER_ID']
-            +",  Number of Approval: " + data['Query'][0][0]['Row' + str(no_of_rows)]['PERFORMNC']
-            +",  Branch Name " + data['Query'][0][0]['Row' + str(no_of_rows)]['BRANCH_NAME']
-            +",  Requested_Amount: " + data['Query'][0][0]['Row' + str(no_of_rows)]['REQUESTED_AMOUNT']
-            +",  Approved_Amount: " + data['Query'][0][0]['Row' + str(no_of_rows)]['APPROVE_AMOUNT'] + "     "
+            final_speech = speech_counter + " User ID: " + data['Query'][0][0]['Row' + str(no_of_rows)]['USER_ID']
+            final_speech=final_speech+",  Number of Approval: " + data['Query'][0][0]['Row' + str(no_of_rows)]['PERFORMNC']
+            final_speech=final_speech+",  Branch Name " + data['Query'][0][0]['Row' + str(no_of_rows)]['BRANCH_NAME']
+            final_speech=final_speech+",  Requested_Amount: " + data['Query'][0][0]['Row' + str(no_of_rows)]['REQUESTED_AMOUNT']
+            final_speech=final_speech+",  Approved_Amount: " + data['Query'][0][0]['Row' + str(no_of_rows)]['APPROVE_AMOUNT'] + "     "
+            speech_counter=final_speech
 
 
 
