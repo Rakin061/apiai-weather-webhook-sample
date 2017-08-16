4e364d78d987281f115fd6a1301a9b08ec8adb49
Rakin <salmankaderrakin@gmail.com>
2017-08-16 11:59:18 +0600

    TOP COMMIT4

--- app.py
+++ app.py
@@ -718,8 +718,8 @@ def processRequest(req):
         speech_counter = ""
         final_speech = ""
 
-        for i in range(1, top_factor + 1):
-            final_speech = speech_counter +" "+i+"."" User ID: " + data['Query']['Row' + str(no_of_rows)]['USER_ID']
+        for i in range(1, top_factor+ 1):
+            final_speech = speech_counter +" "+i+". User ID: " + data['Query']['Row' + str(no_of_rows)]['USER_ID']
             final_speech = final_speech + ",  Number of Approval: " + data['Query']['Row' + str(no_of_rows)][
                 'PERFORMNC']
             final_speech = final_speech + ",  Branch Name " + data['Query']['Row' + str(no_of_rows)]['BRANCH_NAME']
