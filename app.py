7c4f7792bb28d35dcb264fbb7b159bce204c802e
Rakin <salmankaderrakin@gmail.com>
2017-08-16 12:17:57 +0600

    TOP COMMIT10

--- app.py
+++ app.py
@@ -719,13 +719,13 @@ def processRequest(req):
         final_speech = ""
 
         for i in range(1, no_of_rows+ 1):
-            final_speech = speech_counter +" User ID: " + data['Query']['Row' + str(no_of_rows)]['USER_ID']
-            final_speech = final_speech + ",  Number of Approval: " + data['Query']['Row' + str(no_of_rows)][
+            final_speech = speech_counter +" User ID: " + data['Query']['Row' + str(i)]['USER_ID']
+            final_speech = final_speech + ",  Number of Approval: " + data['Query']['Row' + str(i)][
                 'PERFORMNC']
-            final_speech = final_speech + ",  Branch Name " + data['Query']['Row' + str(no_of_rows)]['BRANCH_NAME']
-            final_speech = final_speech + ",  Requested_Amount: " + data['Query']['Row' + str(no_of_rows)][
+            final_speech = final_speech + ",  Branch Name " + data['Query']['Row' + str(i)]['BRANCH_NAME']
+            final_speech = final_speech + ",  Requested_Amount: " + data['Query']['Row' + str(i)][
                 'REQUESTED_AMOUNT']
-            final_speech = final_speech + ",  Approved_Amount: " + data['Query']['Row' + str(no_of_rows)][
+            final_speech = final_speech + ",  Approved_Amount: " + data['Query']['Row' + str(i)][
                 'APPROVE_AMOUNT'] + "     "
             speech_counter = final_speech
 
