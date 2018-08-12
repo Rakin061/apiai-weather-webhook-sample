4c285af1a613673ea36b3d5d9aa445f91ddf02b4
Rakin061 <salmankaderrakin@gmail.com>
2018-08-12 17:17:04 +0600

    Lv.App.02.03

--- app.py
+++ app.py
@@ -660,7 +660,7 @@ def processRequest(req):
                 "speech": "No context named emp_id found. So, I can't proceed. Please contact developer."
             }
         else:
-            emp_id=cont[0]['parameters']['emp_id.original']
+            emp_id=cont[i]['parameters']['emp_id.original']
 
         print("Employee id:-",emp_id)
 
@@ -717,7 +717,7 @@ def processRequest(req):
                 "speech": "No context named emp_id found. So, I can't proceed. Please contact developer."
             }
         else:
-            emp_id = cont[0]['parameters']['emp_id.original']
+            emp_id = cont[i]['parameters']['emp_id.original']
 
         print("Employee id:-", emp_id)
 
