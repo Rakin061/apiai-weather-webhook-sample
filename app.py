1978f20d8ba4dcbf5d37d2311925e6a4208f1031
Rakin061 <salmankaderrakin@gmail.com>
2018-09-16 11:22:53 +0600

    update -3

--- app.py
+++ app.py
@@ -1419,7 +1419,7 @@ def makeWebhookResult(data):
 
 
 
-    speech = " Hellooooo Tahsin!! Today the weather in " + location.get('city') + " is : " + condition.get('text') + \
+    speech = " Hellooooo Salman !!! Today the weather in " + location.get('city') + " is : " + condition.get('text') + \
              ", and the temperature is " + temp + " " + "C" + ".  Thanks!!"
 
     #print("Response:")
